435a6a3e3cb363a893579ebd1ba73c10f5c98e64
BlyatmanCZ <bluemancz2@gmail.com>
2022-04-29 10:53:48 +0200

    FinalProject

---
+++ EndeProject/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Random rn = new Random();
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("                                      --------- SPACEVENTURES ---------");
+		System.out.println("Your galaxy was invaded by evil aliens and you will have to save as many planets as you can to help restore the peace. ");
+		
+		
+		System.out.println("Enter your name:");
+		String name = sc.next();
+		System.out.println("Welcome, " + name);
+		
+		
+	}
+}
