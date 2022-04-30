f06474090a18dd2fbbd09577b0859a190baf8778
BlyatmanCZ <bluemancz2@gmail.com>
2022-04-30 10:14:00 +0200

    Test Change

--- Main.java
+++ Main.java
@@ -1,5 +1,22 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+import java.util.Random;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+        public class Main {
+
+            public static void main(String[] args) {
+
+                Random rn = new Random();
+                Scanner sc = new Scanner(System.in);
+
+                System.out.println("                                      --------- SPACEVENTURES ---------");
+                System.out.println("Your galaxy was invaded by evil aliens and you will have to save as many planets as you can to help restore the peace. ");
+
+
+                System.out.println("\nEnter your name:");
+                String name = sc.next();
+                System.out.println("Welcome, " + name);
+
+
+            }
+        }
