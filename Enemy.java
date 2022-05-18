c2a4b5167f4445bfd356d23d7c5250ed91937b7a
BlyatmanCZ <104194245+BlyatmanCZ@users.noreply.github.com>
2022-05-18 12:51:02 +0200

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -1,4 +1,8 @@
+import java.util.Random;
+
 public class Enemy extends Character{
+	
+	Random rn = new Random();
 
     private String name;
 
@@ -14,6 +18,10 @@ public class Enemy extends Character{
     public void setName(String name) {
         this.name = name;
     }
-
-
+    
+    public Enemy[] enemies = { new Enemy(rn.nextInt(10)+10, rn.nextInt(10)+5, "Alien soldier"), new Enemy(rn.nextInt(7)+15, rn.nextInt(7)+10, "Enemy scientist"),
+    		new Enemy(rn.nextInt(5)+13,rn.nextInt(10)+5, "Alien hound"), new Enemy(rn.nextInt(10)+17, rn.nextInt(10)+5, "Enemy machine"),
+    		new Enemy(rn.nextInt(7)+15, rn.nextInt(10)+5, "Enemy Juggernaut"), new Enemy(rn.nextInt(10)+20,rn.nextInt(5)+5,"Unkown alien")
+    		
+    };
 }
