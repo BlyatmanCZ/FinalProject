30dfea4d18f9c2d6a4ba040ae2cf2f1ec2171053
BlyatmanCZ <104194245+BlyatmanCZ@users.noreply.github.com>
2022-05-18 12:51:39 +0200

    Update Planet.java

--- Planet.java
+++ Planet.java
@@ -16,15 +16,15 @@ public class Planet {
     }
 
     static String[] planetDesc = {"It's freezing cold here. After arriving, you try to look around but all you can see is ice.",
-            "This planet is covered with lava and volcanoes, with average temperature of " + (rn.nextInt(100)+50) + "°C, " +
+            "This planet is covered with lava and full of volcanoes, with average temperature of " + (rn.nextInt(100)+50) + "°C, " +
             "luckily your special suit allows you to move freely on this planet.",
             "This planet is "+(rn.nextInt(30)+70)+"% covered with water. You see some lights coming from the depths of the water.",
-            "This planet is covered with forests. The vegetation looks very different from Earth's however.",
+            "This planet is covered with forests. The vegetation looks very different from Earth's though.",
             "Upon arriving, you notice beautiful rings surrounding this planet.",
             "One of the most developed civilizations once lived on this planet. Now, all you can see is ruins.",
-            "One night on this planet lasts over "+ (rn.nextInt(400)+100) +" Earth days. " +
-            "The only source of light are the planet's "+ (rn.nextInt(8)+2) + " moons.",
-            "You hear a lot of wierd sounds after arriving to this planet."
+            "One night on this planet lasts over "+ (rn.nextInt(100)+1) +" Earth days. " +
+            "The only source of light are the planet's "+ (rn.nextInt(50)+2) + " moons.",
+            "You've heard a lot of wierd sounds after arriving to this planet."
 
     };
 
