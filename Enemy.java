5436bddda4e7fb6cb0f5a5bbd6c2441ba14f58e7
BlyatmanCZ <104194245+BlyatmanCZ@users.noreply.github.com>
2022-05-27 11:03:59 +0200

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -1,8 +1,10 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Enemy extends Character{
 	
-	Random rn = new Random();
+	static Random rn = new Random();
+	static Scanner sc = new Scanner(System.in);
 
     private String name;
 
@@ -19,9 +21,35 @@ public class Enemy extends Character{
         this.name = name;
     }
     
-    public Enemy[] enemies = { new Enemy(rn.nextInt(10)+10, rn.nextInt(10)+5, "Alien soldier"), new Enemy(rn.nextInt(7)+15, rn.nextInt(7)+10, "Enemy scientist"),
+    static public Enemy[] enemies = { new Enemy(rn.nextInt(10)+10, rn.nextInt(10)+5, "Alien soldier"), new Enemy(rn.nextInt(7)+15, rn.nextInt(7)+10, "Enemy scientist"),
     		new Enemy(rn.nextInt(5)+13,rn.nextInt(10)+5, "Alien hound"), new Enemy(rn.nextInt(10)+17, rn.nextInt(10)+5, "Enemy machine"),
     		new Enemy(rn.nextInt(7)+15, rn.nextInt(10)+5, "Enemy Juggernaut"), new Enemy(rn.nextInt(10)+20,rn.nextInt(5)+5,"Unkown alien")
     		
     };
+    
+    static public void battle() {
+    	int rnEnemyNum = rn.nextInt(enemies.length);
+    	
+    	System.out.println("You see a " + enemies[rnEnemyNum].getName() + " in the wild ");
+        System.out.println("\nWhat do you want to do?");
+        
+        System.out.println("1) Attack \n2) Health potion \n3) Strenght potion \n4) Run away");
+        int attChoice = sc.nextInt();
+        switch (attChoice) {
+        	case 1:
+        		System.out.println("You have decided to attack the enemy.");
+        		break;
+        	case 2:
+        		
+        		break;
+        	case 3:
+        		
+        		break;	
+        	case 4:
+        		System.out.println("You have decided to run away.");
+        		break;
+        }
+        
+    }
+  
 }
